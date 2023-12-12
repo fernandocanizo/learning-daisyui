976d7ff9511ab2a46dc81ba4081c6d3935f6041d
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-12-12 14:36:37 -0300

    Show plain TailwindCss and DaisyUI button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,6 +9,18 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <button className="btn">Hello daisyUI</button>
+    <>
+      <div>
+        <label>A DaisyUI button</label>
+        <button className="btn btn-primary">Hello Daisy</button>
+      </div>
+      <div>
+        <label>A plain TailwindCss button</label>
+        <button
+          className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+          Plain TailwindCss
+        </button>
+      </div>
+    </>
   );
 }
