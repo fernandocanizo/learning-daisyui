eb9d68cf8be6f1d56b82eb67ebfa81c6354dce08
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-12-12 15:45:54 -0300

    Fix routes
    Forgot to change dots into slashes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,26 +16,26 @@ export default function Index() {
       <h2 className="flex justify-center text-slate-900 text-xl tracking-tight font-bold mb-3 dark:text-slate-200">Plain DaisyUi</h2>
       <ul>
         <li>
-          <NavLink to="/daisy.login" className="link-primary link-hover">Login</NavLink>
+          <NavLink to="/daisy/login" className="link-primary link-hover">Login</NavLink>
         </li>
         <li>
-          <NavLink to="/daisy.horizontal-menu" className="link-primary link-hover">Horizontal menu</NavLink>
+          <NavLink to="/daisy/horizontal-menu" className="link-primary link-hover">Horizontal menu</NavLink>
         </li>
         <li>
-          <NavLink to="/daisy.sidenav" className="link-primary link-hover">SideNav</NavLink>
+          <NavLink to="/daisy/sidenav" className="link-primary link-hover">SideNav</NavLink>
         </li>
       </ul>
 
       <h2 className="flex justify-center text-slate-900 text-xl tracking-tight font-bold mb-3 dark:text-slate-200">React DaisyUi</h2>
       <ul>
         <li>
-          <NavLink to="/react-daisy.login" className="link-primary link-hover">Login</NavLink>
+          <NavLink to="/react-daisy/login" className="link-primary link-hover">Login</NavLink>
         </li>
         <li>
-          <NavLink to="/react-daisy.horizontal-menu" className="link-primary link-hover">Horizontal menu</NavLink>
+          <NavLink to="/react-daisy/horizontal-menu" className="link-primary link-hover">Horizontal menu</NavLink>
         </li>
         <li>
-          <NavLink to="/react-daisy.sidenav" className="link-primary link-hover">SideNav</NavLink>
+          <NavLink to="/react-daisy/sidenav" className="link-primary link-hover">SideNav</NavLink>
         </li>
       </ul>
     </div>
