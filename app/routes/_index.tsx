00bb20fbce1c9d1ee554f50d1f193de0947f9c74
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-12-12 15:39:07 -0300

    Style index page
    Use Daisy styles for navigation links.
    
    Couldn't find the appropriate class to style headers, so I went back to
    Tailwind utility classes. (awful)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,30 +12,30 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div>
-      <h1>Demo plain DaisyUi vs React-Daisy</h1>
-      <h2>Plain DaisyUi</h2>
+      <h1 className="flex justify-center text-3xl sm:text-4xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 m-3">Demo plain DaisyUi vs React-Daisy</h1>
+      <h2 className="flex justify-center text-slate-900 text-xl tracking-tight font-bold mb-3 dark:text-slate-200">Plain DaisyUi</h2>
       <ul>
         <li>
-          <NavLink to="/daisy.login">Login</NavLink>
+          <NavLink to="/daisy.login" className="link-primary link-hover">Login</NavLink>
         </li>
         <li>
-          <NavLink to="/daisy.horizontal-menu">Horizontal menu</NavLink>
+          <NavLink to="/daisy.horizontal-menu" className="link-primary link-hover">Horizontal menu</NavLink>
         </li>
         <li>
-          <NavLink to="/daisy.sidenav">SideNav</NavLink>
+          <NavLink to="/daisy.sidenav" className="link-primary link-hover">SideNav</NavLink>
         </li>
       </ul>
 
-      <h2>React Daisy</h2>
+      <h2 className="flex justify-center text-slate-900 text-xl tracking-tight font-bold mb-3 dark:text-slate-200">React DaisyUi</h2>
       <ul>
         <li>
-          <NavLink to="/react-daisy.login">Login</NavLink>
+          <NavLink to="/react-daisy.login" className="link-primary link-hover">Login</NavLink>
         </li>
         <li>
-          <NavLink to="/react-daisy.horizontal-menu">Horizontal menu</NavLink>
+          <NavLink to="/react-daisy.horizontal-menu" className="link-primary link-hover">Horizontal menu</NavLink>
         </li>
         <li>
-          <NavLink to="/react-daisy.sidenav">SideNav</NavLink>
+          <NavLink to="/react-daisy.sidenav" className="link-primary link-hover">SideNav</NavLink>
         </li>
       </ul>
     </div>
